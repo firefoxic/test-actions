a47c44afd733c2ada3b58e7ff8ebf34e1b54282c
firefoxic <firefoxic.dev@gmail.com>
2023-07-07 23:43:21 +0300

    Fix the task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-export default function () {
+export default function (done) {
 	console.info(process.env.NODE_ENV)
 	if (process.env.CI) {
 		console.info(process.env.CI)
 		console.info(process.env.REPO_NAME)
 	}
+	done()
 }
