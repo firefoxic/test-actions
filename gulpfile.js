21ed1c990be80091c60109cf41c90aaf8021dc87
firefoxic <firefoxic.dev@gmail.com>
2023-07-08 13:36:03 +0300

    Fix action

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ export default function (done) {
 	// process.env.GITHUB_ENV.HELLO = `World`
 	console.info(process.env.NODE_ENV)
 	if (process.env.CI) {
-		console.info(`World`)
+		console.info(`HELLO="World"`)
 	}
 	done()
 }
