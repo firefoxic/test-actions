8c24c75e81b328633344bf2c9a0b44d6e79094bf
firefoxic <firefoxic.dev@gmail.com>
2023-07-09 16:08:46 +0300

    Fix action

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ export default function (done) {
 	// process.env.HELLO = `World`
 	// console.info(`Test`)
 	if (process.env.CI) {
-		process.env.ACTIONS_ALLOW_UNSECURE_COMMANDS = true
+		process.env[`ACTIONS_ALLOW_UNSECURE_COMMANDS`] = true
 		console.log(`::set-env name=HELLO::${process.env[`HELLO`]}`)
 		// process.env.GITHUB_ENV = `${process.env.GITHUB_ENV}\nHELLO=World`
 		// console.info(process.env.GITHUB_ENV)
