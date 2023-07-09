025493c2b39ecd06369b1ace3de3d7d775998fb5
firefoxic <firefoxic.dev@gmail.com>
2023-07-09 17:10:32 +0300

    Fix action

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
+import { readFile, writeFile } from "node:fs/promises"
+
 export default async function () {
-	console.log(process.env.CI ? `::set-env name=HELLO::World` : `It's not a CI environment`)
-	console.log(process.env.CI ? process.env.GITHUB_ENV : ``)
+	// console.log(process.env.CI ? `::set-env name=HELLO::World` : `It's not a CI environment`)
+	const envFilePath = process.env.GITHUB_ENV
+	const envFileContent = await readFile(process.env.GITHUB_ENV, { encoding: 'utf8' })
+	await writeFile(envFilePath, `${envFileContent}\nHELLO=World`)
+	// console.log(process.env.CI ? process.env.GITHUB_ENV : ``)
 }
