864a7cf7e1c21badac3e7e2a3492e625e9569bd6
firefoxic <firefoxic.dev@gmail.com>
2023-07-09 16:48:22 +0300

    Fix action

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
 export default async function () {
 	console.log(process.env.CI ? `::set-env name=HELLO::World` : `It's not a CI environment`)
-	console.log(process.env.CI ? process.env.ENV_FILE_PATH : ``)
+	console.log(process.env.CI ? process.env.GITHUB_ENV : ``)
 }
