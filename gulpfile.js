1d6c274234ecdb1f3554e8128310da2a2e158406
firefoxic <firefoxic.dev@gmail.com>
2023-07-09 17:38:33 +0300

    Fix action

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 import { readFile, writeFile } from "node:fs/promises"
 
+/**
+ * Set CI environment variable for the GitHub environment file.
+ * @param {string} varName - The name of the environment variable.
+ * @param {string} varValue - The value of the environment variable.
+ */
+async function setEnvVar (varName, varValue) {
+	if (process.env.CI) {
+		const envFilePath = process.env.GITHUB_ENV
+		const envFileContent = await readFile(process.env.GITHUB_ENV, { encoding: 'utf8' })
+		await writeFile(envFilePath, `${envFileContent}\n${varName}=${varValue}`)
+	} else {
+		console.log(`It's not a CI environment`)
+	}
+}
+
 export default async function () {
-	// console.log(process.env.CI ? `::set-env name=HELLO::World` : `It's not a CI environment`)
-	const envFilePath = process.env.GITHUB_ENV
-	const envFileContent = await readFile(process.env.GITHUB_ENV, { encoding: 'utf8' })
-	await writeFile(envFilePath, `${envFileContent}\nHELLO=World`)
-	// console.log(process.env.CI ? process.env.GITHUB_ENV : ``)
+	await setEnvVar(`HELLO`, `World`)
 }
